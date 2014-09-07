ceacd3db79248d04559d584d5e97f0ffb525022b
Fntsrlike <fntsrlike@gmail.com>
2014-09-08 02:08:49 +0800

    Add Restful Controllers and Models with basic feature

---
+++ app/models/ApplyRecord.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
+class ApplyRecord extends Eloquent {
+
+	use SoftDeletingTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'apply_records';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array();
+
+}
